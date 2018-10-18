350e2eef2f71759dc7ad852c31464c9094aa0a76
abdalla12345678 <a.ahmed24@share.epsb.ca>
2018-10-18 13:25:19 -0600

    changes

--- Scripts/LandscapePortrait.js
+++ Scripts/LandscapePortrait.js
@@ -1,1 +1,48 @@
 console.log("You have made it to LandscapePortrait.js");
+
+let stop = false;
+
+function testNaN (number) {
+console.log(stop);
+if(isNaN (number) ) {
+  stop = true;
+  return "Type a real Number"
+} else {
+  return "Input Validate"
+}
+
+function geometry (width, height) {
+if (width == height )
+function main () {
+  let firstNumber, secondNumber, text;
+
+
+  firstNumber = document.getElementById("textfield1").value;
+
+
+
+document.getElementById("validityTest1").innerHTML = alert( testNaN (firstNumber) );
+document.getElementById("validityTest1").innerHTML = testNan (firstNumber);
+
+
+secondNumber = document.getElementById("textfield2").value;
+
+
+document.getElementById("validityTest2").InnerHTML = alert(testNaN (secondNumber) );
+document.getElementById("validityTest2").InnerHTML = testNaN (secondNumber) ;
+
+console.log(stop)
+
+
+if (stop == true) {
+  document.getElementById("large").innerHTMK = "Restart because I said so."
+}
+
+else {
+  console.log("What did you say?", geometry (firstNumber, secondNumber));
+  document.getElementById("large").innerHTML = "What did you say?" + geometry (firstNumber, secondNumber);
+
+}
+let { firstNumber, secondNumber, text);
+console.log(stop)
+}
--- Scripts/childTemplate.js
+++ Scripts/childTemplate.js
@@ -1,1 +1,30 @@
 console.log("You made it to the childTemplate.js!")
+
+function main () {
+
+  let firstNumber, secondNumber, text;
+
+
+  firstNumber = document.getElementById("textfield1").value;
+
+
+
+document.getElementById("validityTest1").innerHTML = alert( testNaN (firstNumber) );
+document.getElementById("validityTest1").innerHTML = testNan (firstNumber);
+
+
+secondNumber = document.getElementById("textfield2").value;
+
+
+document.getElementById("validityTest2").InnerHTML = alert(testNaN (secondNumber) );
+document.getElementById("validityTest2").InnerHTML = testNaN (secondNumber) ;
+
+console.log(stop);
+
+if (stop == true) {
+  document.getElementById("large").innerHTMK = "Restart because I said so."
+}
+
+else {
+  console.log("What did you say?", geometry (firstNumber, secondNumber));
+  document.getElementById("large").innerHTML = "What did you say?" + geometry (firstNumber, secondNumber);
